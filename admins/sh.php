fe86384b385d8c94accf7429d365bd631a6daa38
6232 <2623350233@qq.com>
2021-11-05 13:35:57 +0800

    后台部分全部成功但是还没写布局

--- admins/sh.php
+++ admins/sh.php
@@ -1,9 +1,10 @@
 <?php
 if($_SERVER['REQUEST_METHOD']=="POST"){
-	$jwt=$_SERVER['HTTP_X_TOKEN'];
+	$jwt=$_SERVER['HTTP_TOKEN'];
 	require('vaidata.php');
 	if(vaildata($jwt)=="success"){
 		$messageId=$_POST['messageId'];
+		include("conn.php");
 		$rs=mysqli_query($conn,"update messages set sh=1 where messageId='$messageId'");
 		if($rs){
 			echo '{"status":"10001","msg":"success"}';
